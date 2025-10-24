aa0e57441c228f5e300923941c891eb25b096beb
Anna Clara Lima <anna.lima0304@gmail.com>
2025-10-24 00:10:29 +0000

    feat aula2025-23-10

---
+++ back/routes/alunos.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+// BANCO DE DADOS
+alunos = [
+  {nome: "henning", idade: 40, matricula: 123456}
+]
+
+/* GET alunos API. */
+router.get('/:id', function(req, res, next) {
+  dados = req.params.id
+  console.log('veio', dados)
+  res.send( { alunos });
+});
+
+/* GET alunos pelo ID API. */
+router.get('/', function(req, res, next) {
+  res.send( { alunos });
+});
+
+/* POST alunos API. */
+router.post('/', function(req, res, next) {
+  dados = req.body
+  console.log('veio', dados)
+  res.send( { alunos });
+});
+
+module.exports = router;
